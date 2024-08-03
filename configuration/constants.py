5e80b17cf9c5d64c3423bf0ea294205f82c5f9b7
Denys K <denys.kibi@gmail.com>
2024-08-03 20:08:24 +0300

    ref: removed Yolo weight from the last event to lighten the project

--- configuration/constants.py
+++ configuration/constants.py
@@ -12,6 +12,4 @@ FILE_TEMPLATES_DIR_PATH = 'configuration/file_templates'
 CONFIG_PATH = f'{FILES_DIR_PATH}/config.yaml'
 CONFIG_TEMPLATE_PATH = f'{FILE_TEMPLATES_DIR_PATH}/config_template.yaml'
 YOLO_CONFIG_PATH = './yolov4-tiny/yolov4-tiny-custom.cfg'
-# Default weights: core/weights/default/yolov4-tiny-custom_last.weights
-# Event #1: core/weights/event1/yolov4-tiny-custom_last.weights
-YOLO_WEIGHTS_PATH = 'core/weights/default/yolov4-tiny-custom_last.weights'
+YOLO_WEIGHTS_PATH = 'core/yolo-weights/yolov4-tiny-custom_last.weights'
